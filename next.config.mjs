c5474df67d99e0403ed2f6cb12de0a430ec38485
Skythrill256 <anjishnuganguly773@gmail.com>
2024-12-04 12:14:41 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ import createMDX from '@next/mdx';
 import { remarkCodeHike, recmaCodeHike } from 'codehike/mdx';
 import rehypeSlug from 'rehype-slug';
 // Import the JSON file
-const docsConfig = require('./configs/docs.json');
+import docsData from './configs/docs.json' assert { type: 'json' };
 // import dataArray from './configs/docs';
 
 const { dataArray } = docsData;
